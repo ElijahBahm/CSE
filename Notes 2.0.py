aef402f28aa0309ca725e673f0c0c2b760bf168b
ElijahBahm <33767546+ElijahBahm@users.noreply.github.com>
2017-11-28 15:41:09 -0800

    added Functions

--- Notes 2.0.py
+++ Notes 2.0.py
@@ -23,10 +23,37 @@ print("My car is the %s." % car_name)
 print("My car is the %s. It is a %s." % (car_name, car_type))
 
 # Taking input
-name = input("What is your name?")
-print("Hello %s." % name)
-print(name)
+# name = input("What is your name?")
+# print("Hello %s." % name)
+# print(name)
 
-age = input("What is your age?")
-print("Goodbye is %s" % age)
-print("You belong in a museum!")
+# age = input("What is your age?")
+# print("Goodbye is %s" % age)
+# print("You belong in a museum!")
+
+# Functions
+
+
+def print_hw() :
+    print("Hello World")
+
+
+print_hw()
+
+
+def say_hi(name):
+    print("Hello %s." % name)
+    print("I hope you have a good day")
+
+
+say_hi("Sodapop")
+
+
+def birthday(age):
+    age += 1  # age = age + 1
+    print(age)
+
+
+say_hi("Sodapop")
+print("Soda is 15. Next year:")
+birthday(15)
