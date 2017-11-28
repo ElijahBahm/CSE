eb2fa9f7922e583e461f8c4c9917fd07c1d0b644
ElijahBahm <33767546+ElijahBahm@users.noreply.github.com>
2017-11-28 15:07:19 -0800

    Adding Previous Notes
    11/27/17

---
+++ Notes 2.0.py
@@ -0,0 +1,32 @@
+print("Hello World")
+
+# Elijah Bahm
+
+print(3+5)
+print(5-3)
+print(6/2)
+print(5*3)
+print(3**2)
+
+print()
+print("See if you can figure this out")
+print(600 % 19)
+
+# Variables
+car_name = "Wiebe Mobile"
+car_type = "Lamborghini Aventador"
+car_cylinders = 12
+car_mpg = 9000.1
+
+# Inline Printing
+print("My car is the %s." % car_name)
+print("My car is the %s. It is a %s." % (car_name, car_type))
+
+# Taking input
+name = input("What is your name?")
+print("Hello %s." % name)
+print(name)
+
+age = input("What is your age?")
+print("Goodbye is %s" % age)
+print("You belong in a museum!")
