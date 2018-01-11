ab496a333b1fc693836893e15ff71c714a3a70fa
ElijahBahm <33767546+ElijahBahm@users.noreply.github.com>
2018-01-11 15:39:35 -0800

    better

--- Lucky 7's.py
+++ Lucky 7's.py
@@ -2,6 +2,7 @@ import random
 
 
 money = 15
+max_money = 15
 rolls = 0
 
 while money > 0:
@@ -11,14 +12,22 @@ while money > 0:
     if dice_total == 7:
         money += 4
         rolls += 1
+        max_money += 4
         print("You won this round.")
-        print("Dollars left %s" %money)
-    elif dice_total !=7:
+        print("Dollars left %s" % money)
+    elif dice_total != 7:
         money -= 1
         rolls += 1
+        max_money -= 1
         print("Sorry.")
-        print("Dollars left %s" %money)
+        print("Dollars left %s" % money)
+
 
 if money == 0:
     print("You're out of here!")
     print(rolls)
+
+# max(max_money, money)
+# print(max(max_money, money))
+# filter(max(max_money, money))
+# print(filter(max(max_money, money)))
