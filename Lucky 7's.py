237a79d3bd291d29ae2c75be5867ff9fc7bc6637
ElijahBahm <33767546+ElijahBahm@users.noreply.github.com>
2018-01-12 15:36:43 -0800

    new

--- Lucky 7's.py
+++ Lucky 7's.py
@@ -10,22 +10,30 @@ while money > 0:
     dice_two = random.randint(1, 6)
     dice_total = dice + dice_two
     if dice_total == 7:
-        money += 4
-        rolls += 1
-        max_money += 4
         print("You won this round.")
         print("Dollars left %s" % money)
+        rolls += 1
+        money += 4
+        if money > max_money:
+
+
+
     elif dice_total != 7:
         money -= 1
         rolls += 1
-        max_money -= 1
         print("Sorry.")
         print("Dollars left %s" % money)
+
+# while max_money >= 15:
+#     if max_money >= 15:
+
+
 
 
 if money == 0:
     print("You're out of here!")
     print(rolls)
+
 
 # max(max_money, money)
 # print(max(max_money, money))
