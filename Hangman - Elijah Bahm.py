d596c62183c4d2acf64cbab6eeab1eed09dc59de
ElijahBahm <33767546+ElijahBahm@users.noreply.github.com>
2018-01-23 15:40:56 -0800

    none

--- Hangman - Elijah Bahm.py
+++ Hangman - Elijah Bahm.py
@@ -5,8 +5,8 @@ import random
 Outline of Hangman
 1. Make a word bank - 10 items
 2. Select a random item from the list 
-3. Hide the word (use *)
-4. Reveal letters if they have been guessed
+3. Add user input to a list of letters_guessed
+4. Build a list of letters to show, and display the string form
 5. Create the Win condition
 """
 
@@ -14,10 +14,15 @@ Outline of Hangman
 Fav_Streamers = ["Fugglet", "Shroud", "HCJustin", "Box-Box", "ChocoTaco", "Cyanide Plays Games",
                  "Soviet Womble", "Achievement Hunter", "eMBeaR", "Oompaville", "Kugo"]
 guess = "0"
-guesses = 0
+guesses_left = 10
 word = random.choice(Fav_Streamers)
-letter = {"%s" % word}
-letters_guessed = "a"
+hidden_word = ["%s" % word]
+letters_guessed = []
 
-while guess != letter and guesses < 10:
+while guesses_left > 0:
     guess = input("Choose a letter, to save your Friend.")
+    if guess == letter in hidden_word:
+
+
+    for letter in hidden_word:
+
