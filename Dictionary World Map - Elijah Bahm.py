09a966d6e1db9948591b7d59d98d0033d54ced08
ElijahBahm <33767546+ElijahBahm@users.noreply.github.com>
2018-02-14 15:40:23 -0800

    LOL

--- Dictionary World Map - Elijah Bahm.py
+++ Dictionary World Map - Elijah Bahm.py
@@ -1,28 +1,43 @@
 world_map = {
-    'WESTHOUSE':{
+    'WESTHOUSE': {
         "NAME": 'WEST OF HOUSE',
         'DESCRIPTION': 'You are west of a small house.',
-        'PATHS':{
+        'PATHS': {
             'NORTH': 'NORTHHOUSE',
             'SOUTH': "SOUTHHOUSE"
         }
     },
-    'NORTHHOUSE':{
+    'NORTHHOUSE': {
         "NAME": 'NORTH OF HOUSE',
         'DESCRIPTION': 'You are north of a small house.',
-        'PATHS':{
+        'PATHS': {
             'WEST': "WESTHOUSE"
         }
     },
-    'SOUTHHOUSE':{
+    'SOUTHHOUSE': {
         'NAME': 'SOUTH OF HOUSE',
         'DESCRIPTION': 'You are south of a small house.',
-        'PATHS':{
+        'PATHS': {
             'WEST': "WESTHOUSE"
         }
     }
 }
 
 current_node = world_map['SOUTHHOUSE']
-print(current_node['NAME'])
-print(current_node['DESCRIPTION'])
+directions = ['NORTH', 'SOUTH', 'EAST', 'WEST']
+
+while True:
+    print(current_node["NAME"])
+    print(current_node["DESCRIPTION"])
+    command = input('>_')
+    if command == 'quit':
+        quit(0)
+    if command in directions:
+        try:
+            name_of_node = current_node['PATHS'][command]
+            current_node = world_map[name_of_node]
+        except KeyError:
+            print('You cannot got that way')
+    else:
+        print('Command Not Recognized.')
+    print()
--- Game World Map - Elijah Bahm.py
+++ Game World Map - Elijah Bahm.py
@@ -4,13 +4,14 @@ world_map = {
         'DESCRIPTION': 'You are in a big open square in the center of this town. The big sign in front of you '
                        'says WELCOME TO DUST',
         'PATHS': {
-            'NORTH': 'NORTHHOUSE',
-            'SOUTH': 'SOUTHHOUSE'
+            'NORTH': 'TOWNHALL',
+            'WEST': 'MARKET'
+            ''
         }
     },
     'MARKET': {
         'NAME': 'THE MARKET',
-        'DESCRIPTION': 'You are in what used to be a giant bazaar, now there is only one vendor open. /n His shop'
+        'DESCRIPTION': 'You are in what used to be a giant bazaar, now there is only one vendor open. \n His shop'
                        ' is only slightly nicer than the than the abandoned ones, mainly just less dusty.',
         'PATHS': {
             'WEST': 'WESTHOUSE'
@@ -18,7 +19,7 @@ world_map = {
     },
     'TOWNHALL': {
         'NAME': 'TOWN HALL',
-        'DESCRIPTION': 'You are in a very long, dark, and tall building. It has a small display of the '
+        'DESCRIPTION': 'You are in a very long, dark, and tall building. It has a small display of the \n '
                        'founding of the town and on of something like a teleporter that needs a special key to use.',
         'PATHS': {
             'WEST': "WESTHOUSE"
@@ -26,12 +27,28 @@ world_map = {
     },
     'PUBLICRESTROOM': {
         'NAME': 'THE ONE PUBLIC RESTROOM',
-        'DESCRIPTION': ''
-
-
-
+        'DESCRIPTION': '',
+        'PATHS': {
+            'SOUTHEAST': 'TOWNSQUARE'
+        }
     }
 }
 
+current_node = world_map['TOWNSQUARE']
+directions = ['NORTH', 'SOUTH', 'EAST', 'WEST', 'NORTHWEST', 'NORTHEAST', 'SOUTHEAST', 'SOUTHWEST']
 
-print(world_map['MARKET']['DESCRIPTION'])
+while True:
+    print(current_node["NAME"])
+    print(current_node["DESCRIPTION"])
+    command = input('>_')
+    if command == 'quit':
+        quit(0)
+    if command in directions:
+        try:
+            name_of_node = current_node['PATHS'][command]
+            current_node = world_map[name_of_node]
+        except KeyError:
+            print('You cannot got that way')
+    else:
+        print('Command Not Recognized.')
+    print()
