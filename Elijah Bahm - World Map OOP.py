d9d64c1096aa2d919de3fa4ae45c0c4c7f6859f5
ElijahBahm <33767546+ElijahBahm@users.noreply.github.com>
2018-03-02 15:36:16 -0800

    l

--- Elijah Bahm - World Map OOP.py
+++ Elijah Bahm - World Map OOP.py
@@ -30,5 +30,32 @@ ride_station = Room("Catch-A-Ride-Station", "badlands", "worlds_end", "death", "
                     'You are in a small area that is covered with a tin roof and has a terminal, that you \n assume will either teleport you or spawn something for you.')
 badlands = Room("The Badlands", None, "ride_station", "suffering", None, None, None, None, None, 'Again, you feel '
                 'regret as soon as you come here, as soon as the eye can see there are \n just heads on bloody stakes '
-                'and bodies strung up with chains every where else in the area. \n And, yet again, you feel that there will be something here.')
-suffering = Room("Suffering", None, "")
+                'and bodies strung up with chains every where else in the area. And, yet again, you feel that there will be something here.')
+suffering = Room("Suffering", None, "death", None, "badlands", None, None, None, None, 'You are in the remains of a '
+                 'grand city and the last survivor from this city is an old man, \n who is very weak and malnourished, '
+                 'he is only a couple feet away from you. You hold his life in the palm of your hand.')
+death = Room("Death, The Town", "suffering", "")
+
+current_node = town_square
+directions = ['north', 'south', 'east', 'west', 'northwest', 'northeast', 'southeast', 'southwest']
+short_directions = ['n', 's', 'e', 'w', 'nw', 'ne', 'se', 'sw']
+
+while True:
+    print(current_node*****)
+    print(current_node*****)
+    command = input('>_').lower().strip()
+    if command == 'quit':
+        quit(0)
+    elif command in short_directions:
+        # Finds the command in shorts directions (index #)
+        pos = short_directions.index(command)
+        command = directions[pos]
+    if command in directions:
+        try:
+            name_of_node = current_node['PATHS'][command]
+            current_node = world_map[name_of_node]
+        except KeyError:
+            print('Nope, Sorry.')
+    else:
+        print('Command Not Recognized.')
+    print()
