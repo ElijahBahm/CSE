03115ee78bba847f399a57cc4e57a6d3d52fcd3b
ElijahBahm <33767546+ElijahBahm@users.noreply.github.com>
2018-03-20 15:39:17 -0700

    lol

---
+++ Items - Elijah Bahm.py
@@ -0,0 +1,27 @@
+class Item(object):
+    def __init__(self, name, description, use):
+        self.name = name
+        self.description = description
+        self.use = use
+
+
+class Shield(Item):
+    def __init__(self, name, description, use, ):
+        super(Shield, self).__init__(name, description, use)
+        self.
+        self.
+
+
+class Pieces(Item):
+    def __init__(self):
+        super(Pieces, self).__init__()
+        self.
+        self.
+
+
+class Weapon(Item):
+    def __init__(self, ):
+        self.
+        self.
+
+
