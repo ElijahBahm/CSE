59c5c2dd7ac9c7939c5e69f04fa4aefca609cf8e
ElijahBahm <33767546+ElijahBahm@users.noreply.github.com>
2018-03-21 15:39:14 -0700

    lol

--- Items - Elijah Bahm.py
+++ Items - Elijah Bahm.py
@@ -5,23 +5,34 @@ class Item(object):
         self.use = use
 
 
-class Shield(Item):
+class Healing(Item):
     def __init__(self, name, description, use, ):
+        super(Healing, self).__init__(name, description, use)
+
+
+class Shield(Item):
+    def __init__(self, name, description, use, health, ):
         super(Shield, self).__init__(name, description, use)
-        self.
+        self.health = health
         self.
 
 
 class Pieces(Item):
+    def __init__(self, name, description, use, ):
+        super(Pieces, self).__init__(name, description, use)
+        self.
+        self.
+
+
+class Core(Pieces):
     def __init__(self):
-        super(Pieces, self).__init__()
-        self.
-        self.
+        super(Core, self).__init__()
 
 
 class Weapon(Item):
-    def __init__(self, ):
-        self.
+    def __init__(self, name, description, use, brand, ):
+        super(Weapon, self).__init__(name, description, use)
+        self.brand = brand
         self.
 
 
